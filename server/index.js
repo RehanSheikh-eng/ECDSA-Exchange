a027e990ed30ea80e7a99d5d08dc7d4354d65145
Rehan Sheikh <rs2126@cam.ac.uk>
2021-12-16 13:44:54 +0000

    Added private keys to sever

--- server/index.js
+++ server/index.js
@@ -2,17 +2,23 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
+const SHA256 = require('crypto-js/sha256');
+const secp = require("@noble/secp256k1");
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
-}
+// Generate Private keys for 3 users
+let privateKey1 = secp.utils.randomPrivateKey();
+let privateKey2 = secp.utils.randomPrivateKey();
+let privateKey3 = secp.utils.randomPrivateKey();
+
+// Convert keys to hex strings
+privateKey1 = Buffer.from(privateKey1).toString('hex');
+privateKey2 = Buffer.from(privateKey2).toString('hex');
+privateKey3 = Buffer.from(privateKey3).toString('hex');
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
